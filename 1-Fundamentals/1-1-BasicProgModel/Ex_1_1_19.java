e7446e4e2321b2d3e92656f566fc3b97923976a2
hexiongjun <hexiongjun@pset.suntec.net>
2015-07-10 16:59:39 +0800

    Explain the 1.1.19 code
    Change-Id: I3123077cb7c95d581499115fb6cc93bf0dcaa634

--- 1-Fundamentals/1-1-BasicProgModel/Ex_1_1_19.java
+++ 1-Fundamentals/1-1-BasicProgModel/Ex_1_1_19.java
@@ -16,12 +16,18 @@ public class Ex_1_1_19
 
     public static long Fib(int N, long[] f)
     {
+        /*
+         *For java the array value would be initialized to 0
+         * So if the value is not 0, which means it has been computed!
+         */
         if (f[N] == 0)
         {
             if (N == 1)
                 f[N] = 1;
             else if (N > 1)
                 f[N] = Fib(N-1, f) + Fib(N-2, f);
+        } else {
+            //StdOut.println("Already exist!!");
         }
         
         return f[N];
